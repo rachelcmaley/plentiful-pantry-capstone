a33d8f1035388257d3ef4cefbd8d8eb6ad9a5d4c
Austin Jones <jonesaustinn@gmail.com>
2023-01-31 11:25:21 -0800

    pantry and pantry ingredients tables are created

--- meal-planner/java/database/04_constraints.sql
+++ meal-planner/java/database/04_constraints.sql
@@ -1,3 +1,12 @@
 -- add database constraints here - such as foreign key constraints
 -- very common to add constraints AFTER bulk loading data
 
+ALTER TABLE pantry_ingredients
+ADD CONSTRAINT FK_pantry_ingredients_pantry
+FOREIGN KEY (pantry_id)
+REFERENCES pantry(pantry_id);
+
+ALTER TABLE pantry_ingredients
+ADD CONSTRAINT FK_pantry_ingredients_ingredients
+FOREIGN KEY (ingredient_id)
+REFERENCES ingredients(ingredient_id);
