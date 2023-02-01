702a1ce572a8e3be6e88b97aab7149e3ff9e54b4
Austin Jones <jonesaustinn@gmail.com>
2023-01-31 17:06:22 -0800

    create vie page to display list of ingredients in the pantry

---
+++ meal-planner/vue/src/components/IngredientsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ingredients-list">
+    <div v-for="ingredient in ingredients" v-bind:key="ingredient.id" class="ingredient">
+        {{ ingredient.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+    import pantryService from '../services/PantryService.vue';
+
+    export defualt {
+        name: 'ingredient-list',
+        data () {
+            return {
+                ingredients []
+            };
+        },
+
+        created () {
+
+            const pantryPromise = pantryService.getIngredients();
+
+            pantryPromise.then (response => {
+                this.ingredients = response.data
+
+            });
+
+        }
+    };
+
+</script>
