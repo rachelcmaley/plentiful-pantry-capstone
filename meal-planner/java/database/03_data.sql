d9a5a3f9a85202cd4e290387a8cbd50e06fd92db
Rachel Maley <rachelcmaley@gmail.com>
2023-02-01 14:15:04 -0800

    added statement to copy ingredients .csv to database when creating database

--- meal-planner/java/database/03_data.sql
+++ meal-planner/java/database/03_data.sql
@@ -4,5 +4,8 @@ INSERT INTO users (user_id, username,password_hash,role) VALUES (1, 'user', '$2a
 INSERT INTO users (user_id, username,password_hash,role) VALUES (3, 'admin', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 ALTER SEQUENCE seq_user_id RESTART WITH 3;
 
--- insert ??? table data
+-- insert ingredients table data
+COPY ingredients(ingredient_name, ingredient_id)
+FROM 'C:\Users\Student\workspace\capstone-meal-planner\meal-planner\java\top-1k-ingredients.csv'
+DELIMITER ';';
 
