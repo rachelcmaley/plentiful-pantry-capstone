c5ca893571d3e0c590b974bdceb6347f02a51dd5
Joseph Fajardo <JosephFajardo@gmail.com>
2023-02-02 12:42:29 -0800

    Update

---
+++ meal-planner/java/src/main/java/com/techelevator/controller/MealPlanController.java
@@ -0,0 +1,13 @@
+package com.techelevator.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class MealPlanController {
+
+    @Autowired
+    private MealPlanDao mealplanDao;
+}
