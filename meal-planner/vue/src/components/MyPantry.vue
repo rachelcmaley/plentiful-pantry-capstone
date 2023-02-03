4ddbb2cda4b34d054a882f8f8ecc57da7ccd068e
Hailey Cornelius <Haileycornelius20022.gmail.com[D[D[D[D[D[D[D[D[D[D[[[[C[C[C[C[C[C[C[C2@gmail.com>
2023-02-02 18:23:30 -0800

    added a lot of stuff

--- meal-planner/vue/src/components/MyPantry.vue
+++ meal-planner/vue/src/components/MyPantry.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="my-pantry">
-        <h2>My Pantry</h2>
+        <h2 id="pantry">My Pantry</h2>
         <ul>
             <li>eggs</li>
+            <div id="underline"> </div>
             <li>apples</li>
+             <div id="underline"> </div>
             <li>pears</li>
+             <div id="underline"> </div>
             <li>tortillas</li>
+             <div id="underline"> </div>
             <li>beans</li>
+             <div id="underline"> </div>
             <li>oranges</li>
+             <div id="underline"> </div>
             <li>cucumber</li>
+             <div id="underline"> </div>
             <li>meat</li>
+             <div id="underline"> </div>
         </ul>
     </div>
 </template>
@@ -18,5 +26,28 @@
 .my-pantry{
     background: white;
     border-radius: 10px;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-size: 20px;
 }
+
+#underline{
+    border-bottom: solid 2px grey;
+    margin-right: 90px;
+}
+
+#pantry{
+    text-align: center;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    color: green;
+}
+
+#paperLine{
+    border-left: solid rgb(25, 25, 129) 2px;
+    margin-left: 10px;
+}
+
+li{
+    margin-left: 50px;
+}
+
 </style>
--- meal-planner/vue/src/components/RecommendedRecipes.vue
+++ meal-planner/vue/src/components/RecommendedRecipes.vue
@@ -1,15 +1,26 @@
 <template>
 <div class="recommended">
-    <h2>Recommended Recipes</h2>
-    <p>Quesadillas</p>
-    <p>Cereal</p>
-</div>
+    <h2 id="subTitle">Recommended Recipes</h2>
+
+    
+
+    </div>
 
 </template>
 
 <style>
 .recommended{
     background: white;
+    border: solid green 2px;
     border-radius: 10px;
+    
 }
+
+#subTitle{
+    text-align: center;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    color: green;
+}
+
+
 </style>
