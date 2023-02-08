66c7a2fbf8e2c094bbdd4afb96709fa5909b4f9e
Joseph Fajardo <JosephFajardo@gmail.com>
2023-02-07 22:57:39 -0800

    I dont know

--- meal-planner/vue/src/services/RecipesService.js
+++ meal-planner/vue/src/services/RecipesService.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export default {
-    getRecipes(userId) {
-        return axios.get(`/recipes/${userId}`);
+    // getRecipes(userId) {
+    //     return axios.get(`/recipes/${userId}`);
+    // },
+    getRecipes() {
+        return axios.get(`/recipes/1`);
     },
 
     addRecipe(userId, recipeId, recipeName) {
